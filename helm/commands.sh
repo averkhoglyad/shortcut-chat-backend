1bb54f1af5378163ab4756140e5b56ab056b767e
a.v.verkhoglyad <a.v.verkhoglyad@gmail.com>
2025-01-03 21:28:48 +0300

    helm charts fixes and refactoring

---
+++ helm/commands.sh
@@ -0,0 +1,11 @@
+helm install users-pg-data ./mount-volume -f ./users.pg-data-volume.values.yaml
+
+helm install users-db ./postgres-db -f ./users.pg.values.yaml
+
+kubectl port-forward svc/users-db 15432:5432
+
+helm install ./users-app -f ./users.app.values.yaml
+
+kubectl port-forward svc/users-app 8000:8080
+
+
