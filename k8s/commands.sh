dd3b2518fcefad98226e7572725d3c1b6ec96c3f
a.v.verkhoglyad <a.v.verkhoglyad@gmail.com>
2025-01-04 10:22:36 +0300

    apply changes in k8s manifests and helm charts

--- k8s/commands.sh
+++ k8s/commands.sh
@@ -1,3 +1,12 @@
+# Kafka
+kubectl apply -f .\kafka\zookeeper\deployment.yaml \
+              -f .\kafka\zookeeper\service.yaml \
+              -f .\kafka\deployment.yaml \
+              -f .\kafka\service.yaml
+
+kubectl port-forward svc/kafka-service 19092:9092
+
+# UserApp
 kubectl apply -f .\users\config\configmap.yaml \
               -f .\users\config\secret.yaml \
               -f .\users\postgres\volume.yaml \
@@ -10,3 +19,7 @@ kubectl port-forward pods/users-db-statefulset-0 15432:5432
 
 kubectl port-forward svc/users-app-service 8000:8080
 
+# MailSender
+kubectl apply -f .\mail-sender\config\configmap.yaml \
+              -f .\mail-sender\config\secret.yaml \
+              -f .\mail-sender\deployment.yaml
